c8e63b02275f74b343eb9e2f6a9a9dbdbf09b3ee
Davron <absaitovdev@gmail.com34rrg>
2023-08-19 13:26:36 +0500

    button

--- bot/buttons/reply_buttons.py
+++ bot/buttons/reply_buttons.py
@@ -4,3 +4,8 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 async def phone_number():
     design = [[KeyboardButton(text="Telefon raqamni yuborish📲", request_contact=True)]]
     return ReplyKeyboardMarkup(keyboard=design, resize_keyboard=True)
+
+
+async def location():
+    design = [[KeyboardButton(text="Joylashuvni yuborish📍", request_location=True)]]
+    return ReplyKeyboardMarkup(keyboard=design, resize_keyboard=True)
