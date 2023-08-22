2493cb55388ce235476cc262b95556234731a545
Davron <absaitovdev@gmail.com34rrg>
2023-08-22 14:25:28 +0500

    button

--- FoodProject/urls.py
+++ FoodProject/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('category.urls')),
-    # path('', include('orderfood.urls')),
+    path('', include('orderfood.urls')),
     path('', include('post.urls')),
     path('', include('user.urls')),
 ]
--- orderfood/urls.py
+++ orderfood/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from orderfood.views import OrderFoodListView, OrderFoodDetailView, CategoryProductsView
 
 urlpatterns = [
-    path('orderfoods/', OrderFoodListView, name='order-food'),
-    path('orderfood/<int:pk>/', OrderFoodDetailView, name='order-food'),
+    path('orderfoods/', OrderFoodListView.as_view(), name='order-food'),
+    path('orderfood/<int:pk>/', OrderFoodDetailView.as_view(), name='order-food'),
     path('category-food/<int:pk>/', CategoryProductsView.as_view(), name='category-foods')
 ]
